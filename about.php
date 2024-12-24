2c9b4d78062460cb417daee56619ffba00bca015
malak31ayman <malakaiman785@gmail.com>
2024-12-24 21:44:14 +0200

    about

---
+++ about.php
@@ -0,0 +1,185 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>About</title>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css"
+        integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Fredoka:wght@300..700&family=Inconsolata:wght@200..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Fredoka:wght@300..700&family=Inconsolata:wght@200..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+        rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Cinzel+Decorative:wght@400;700;900&family=Fredoka:wght@300..700&family=Inconsolata:wght@200..900&family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Pacifico&display=swap"
+        rel="stylesheet">
+    <link rel="stylesheet" href="about.css" />
+</head>
+
+<body>
+<?php include 'includes/header.php'; ?>
+
+    <div class="background">
+        <h2>About</h2>
+        <p><a href="home.php" class="hover">Home</a> / About</p>
+    </div>
+    <div class="pets">
+        <div class="bunny">
+            <img src="Assets/Images/bunny.jpeg" alt="" id="girl" />
+            <div class="content">
+                <div class="droplet">
+                    <i class="fa-solid fa-droplet"></i>
+                </div>
+                <div class="text">
+                    <h2>Caring Pets Gives Joy & Happiness</h2>
+                    <p id="content1">
+                        Sed vestibulum nulla elementum auctor tincidunt.
+                        Aliquam sit amet cursus mauris. Sed vitae mattis
+                        ipsum. Vestibulum enim nulla, sollicitudin ac
+                        hendrerit nec, tempor quis nisl.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="parrot">
+            <img src="Assets/Images/parrot.jpeg" alt="" />
+            <div class="content">
+                <div class="tag">
+                    <i class="fa-solid fa-tag"></i>
+                </div>
+                <div class="text">
+                    <h2>Healthy & Nutriment Grains For Pets</h2>
+                    <p id="content2">
+                        Nunc sed blandit libero volutpat. Imperdiet massa
+                        tincidunt nunc pulvinar sapien et ligula
+                        ullamcorper. Id consectetur purus ut faucibus
+                        pulvinar elementum integer. Eleifend quam adipiscing
+                        vitae proin.
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="clients">
+        <h4>Customer Review</h4>
+        <h2>Clients Are Saying</h2>
+        <div class="cards">
+            <div class="card">
+                <p>
+                    Sed augue lacus viverra vitae. Semper quis lectus nulla
+                    at volutpat diam ut. Purus non enim praesent elementum
+                    facilisis leo vel fringilla est. Id semper risus in
+                    hendrerit gravida rutrum quisque non tellus. Justo
+                    laoreet sit amet cursus.
+                </p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="person">
+                    <img src="Assets/Images/person1.jpeg" alt="" />
+                </div>
+                <h3 class="name">Elizabeth Johnson</h3>
+                <p class="job">Auditor</p>
+            </div>
+            <div class="card">
+                <p>
+                    Ac orci phasellus egestas tellus rutrum tellus. Dui
+                    vivamus arcu felis bibendum. Cursus risus at ultrices mi
+                    tempus imperdiet nulla malesuada. Lobortis feugiat
+                    vivamus at augue eget arcu. Id donec ultrices tincidunt
+                    arcu non.&nbsp;
+                </p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="person">
+                    <img src="Assets/Images/person6.jpeg" alt="" />
+                </div>
+                <h3 class="name">Jessica Walker</h3>
+                <p class="job">Developer</p>
+            </div>
+            <div class="card">
+                <p>
+                    Tincidunt dui ut ornare lectus sit amet est. Faucibus
+                    ornare suspendisse sed nisi lacus sed viverra. Eget
+                    egestas purus viverra accumsan in nisl nisi. Facilisi
+                    nullam vehicula ipsum a arcu cursus vitae congue
+                    mauris.&nbsp Id donec ultrices tincidunt arcu non.;
+                </p>
+                <div class="stars">
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-solid fa-star"></i>
+                    <i class="fa-regular fa-star"></i>
+                </div>
+                <div class="person">
+                    <img src="Assets/Images/person3.jpeg" alt="" />
+                </div>
+                <h3 class="name">James Cameron</h3>
+                <p class="job">Musician</p>
+            </div>
+        </div>
+    </div>
+    <div class="staff">
+        <h4>Our Staff</h4>
+        <h2>Meet Our Excellent Person</h2>
+        <div class="users">
+            <div class="user">
+                <div class="userImg">
+                    <img src="Assets/Images/person2.jpeg" alt="" />
+                </div>
+                <h2 class="user-name">Victoria Lewis</h2>
+                <p class="user-job">Manager</p>
+            </div>
+            <div class="user">
+                <div class="userImg">
+                    <img src="Assets/Images\abo-08.webp" alt="" />
+                </div>
+                <h2 class="user-name">John Thomas</h2>
+                <p class="user-job">Sales Executive</p>
+            </div>
+            <div class="user">
+                <div class="userImg">
+                    <img src="Assets/Images/person4.jpeg" alt="" />
+                </div>
+                <h2 class="user-name">Edith Underwood</h2>
+                <p class="user-job">Store Keeper</p>
+            </div>
+            <div class="user">
+                <div class="userImg">
+                    <img src="Assets/Images/person7.jpeg" alt="" />
+                </div>
+                <h2 class="user-name">Winston Charles</h2>
+                <p class="user-job">Customer Support</p>
+            </div>
+        </div>
+    </div>
+<?php include 'includes/footer.php'; ?>
+</body>
+
+</html>
