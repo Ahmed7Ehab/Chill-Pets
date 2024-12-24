8dad65750420db6f8ae7e0d6dad9cdef18ee163a
Ahmed7Ehab <ahmedihab712@gmail.com>
2024-12-24 05:16:47 +0200

    categories

--- cat.php
+++ cat.php
@@ -1,3 +1,12 @@
+<?php
+include "init.php";
+session_start();
+$id=$_GET['category_id'];
+$query="SELECT * FROM products WHERE category_id =$id";
+$stmt=$pdo->prepare($query);
+$stmt->execute();
+$products=$stmt->fetchAll();
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,7 +15,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cat - Chill Pets</title>
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <!-- Custom CSS -->
     <link rel="stylesheet" href="cat.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
@@ -31,51 +40,27 @@
     </div>
 </section>
 
+
 <div class="featured_product container my-5">
     <div class="featured-wrapper">
         <!--clickable product-->
-        <a href="product.php" style="text-decoration: none; color: inherit;">
-            <div class="types">
-                <div class="product-img mb-3">
-                    <i class="cart fa-solid fa-cart-shopping"></i>
-                    <img src="Assets/Images/product3.jpg" alt="Healthy Treats">
+        <?php foreach ($products as $product) {?>
+            <a href="product.php?id=<?=$product['id']?>" style="text-decoration: none; color: inherit;">
+                <div class="types">
+                    <div class="product-img mb-3">
+                        <i class="cart fa-solid fa-cart-shopping"></i>
+                        <img src="<?= "storage/".$product['picture']?>" alt="<?=$product['title']?>">
+                    </div>
+                    <h3><?=$product['title']?></h3>
+                    <p><?= "$".$product['price'] ?></p>
+                    <p class="p_featured"><?=$product['p_status']?></p>
                 </div>
-                <h3>Lamb Fillets</h3>
-                <p>RS.80.14$</p>
-                <p class="p_featured">instock</p>
-            </div>
-        </a>
-        <div class="types">
-            <div class="product-img mb-3">
-                <i class="cart fa-solid fa-cart-shopping"></i>
-                <img src="Assets/Images/product4.jpg" alt="Freeze Dried">
-            </div>
-            <h3>Pro-Rich Fish</h3>
-            <p>RS.50$</p>
-            <p class="p_featured">instock</p>
-        </div>
-        <div class="types">
-            <div class="product-img mb-3">
-                <i class="cart fa-solid fa-cart-shopping"></i>
-                <img src="Assets/Images/product16.jpg" alt="Dog">
-            </div>
-            <h3>Discuss Fish</h3>
-            <p>RS.25.00$</p>
-            <p class="p_featured">soldout</p>
-        </div>
-        <div class="types">
-            <div class="product-img mb-3">
-                <i class="cart fa-solid fa-cart-shopping"></i>
-                <img src="Assets/Images/product20_8d6fda60-29ab-4bfb-a3c1-47d515d4a91c.jpg" alt="Cat">
-            </div>
-            <h3>Pupy Dry Formula Food </h3>
-            <p>RS.60.00$</p>
-            <p class="p_featured">instock</p>
-        </div>
-
+            </a>
+        <?php }?>
     </div>
 </div>
 <?php include 'includes/footer.php'; ?>
+
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
